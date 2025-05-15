154fad103c80ed0cd9a462245fb828aba71d7650
gui castaldeli <guicastaldeli8@gmail.com>
2025-05-15 16:28:07 -0400

    update-04_07

--- app/src/global-header.tsx
+++ app/src/global-header.tsx
@@ -3,23 +3,25 @@ import './styles/main/styles.scss';
 export default function GlobalHeader() {
     return (
         <div id="-global-header">
-            {/* Logo */}
-            <div id="--logo-container">
-                <div id="---logo-content">
-                    <p>LOGO</p>
+            <div id="--header-content">
+                {/* Logo */}
+                <div id="---logo-container">
+                    <div id="_logo-content">
+                        <p>LOGO</p>
+                    </div>
                 </div>
-            </div>
 
-            {/* Search Bar */}
-            <div id="--search-bar-container">
-                <div id="---search-bar-content">
-                    <p>search</p>
-                    {/* create searchbartsx later... */}
+                {/* Search Bar */}
+                <div id="---search-bar-container">
+                    <div id="_search-bar-content">
+                        <p>search</p>
+                        {/* create searchbartsx later... */}
+                    </div>
                 </div>
-            </div>
 
-            {/* info */}
-            <div id='--bar-info'>info</div>
+                {/* info */}
+                <div id='---bar-info'>info</div>
+            </div>
         </div>
     )
 }
